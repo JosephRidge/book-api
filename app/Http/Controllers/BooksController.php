bf9fda8dc3e1897e0d952f02d5cc047a11346d4e
josephridge <josephridge18@gmail.com>
2023-11-17 13:02:24 +0300

    Update:

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -8,6 +8,63 @@ use App\Models\Book;
 
 class BooksController extends Controller
 {
+
+    /*
+            - Create new book
+            -  Model 
+            - validating the inputs 
+    */
+    function createNewBook(Request $request){
+       
+        // validation
+       $request->validate([
+        'name'=>'required',
+        'author'=>'required',
+        'pages'=>'required',
+        'IBN'=>'required',
+        'category'=>'required',
+        'publisher'=>'required',
+        'yearOfPublication'=>'required',
+    ]); 
+
+    // creating our record
+    $book = Book::create([
+        'name'=>$request->name,
+        'author'=>$request->author,
+        'pages'=>$request->pages,
+        'IBN'=>$request->IBN,
+        'category'=>$request->category,
+        'publisher'=>$request->publisher,
+        'yearOfPublication'=>$request->yearOfPublication
+    ]);
+
+    // retrieve the book & check if empty
+    $book = Book::find($book->id);
+
+    // we return a response ( -> success or failure )
+    if(!$book){
+        return response([
+            'message'=>'unsuccessful!'
+        ]);
+    }else{
+        return response([
+            'message'=>'success',
+            'book'=>$book
+        ]);
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
     //Create new Book
     function createBook(Request $request){
         // validate inputs
--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/createBook',[BooksController::class,'createBook']);
-
+Route::post('/createBook',[BooksController::class,'createNewBook']);
